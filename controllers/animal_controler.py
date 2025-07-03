fc3c8606c52ad0e03468c8334f8a47bff58efa3a
DonMtys <joaovictor323444@gmail.com>
2025-07-03 18:08:28 -0300

    Corrige AnimalController - adiciona método index e import correto

--- controllers/animal_controler.py
+++ controllers/animal_controler.py
@@ -1,8 +1,11 @@
-
 from bottle import Bottle, template
-from services.animal_service import AnimalService
+from services.animal_servico import AnimalService 
 
 class AnimalController:
     def __init__(self, app: Bottle):
         self.animal_service = AnimalService()
         app.route('/animais', 'GET', self.index)
+    
+    def index(self):
+        animais = self.animal_service.get_all() 
+        return template('animais/index', animais=animais)
